f77ff435c9140ee6346079961c8ff4b897a6bdf3
SuryaKF <suryakf04@gmail.com>
2024-09-02 22:21:50 +0530

    Update the color selctor and the theme button

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,6 +1,8 @@
 const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
+const themeToggle = document.getElementById('theme-toggle');
+const colorThemeSelect = document.getElementById('color-theme');
 
 let allTodos = getTodos();
 updateTodoList();
@@ -9,6 +11,9 @@ todoForm.addEventListener('submit', function(e) {
     e.preventDefault();
     addTodo();
 });
+
+themeToggle.addEventListener('click', toggleTheme);
+colorThemeSelect.addEventListener('change', changeColorTheme);
 
 function addTodo() {
     const todoText = todoInput.value.trim();
@@ -23,6 +28,7 @@ function addTodo() {
         todoInput.value = "";
     }
 }
+
 function updateTodoList() {
     todoListUL.innerHTML = "";
     allTodos.forEach((todo, todoIndex) => {
@@ -31,7 +37,7 @@ function updateTodoList() {
     });
 }
 
-function createTodoItem(todo, todoIndex ) {
+function createTodoItem(todo, todoIndex) {
     const todoId = "todo-"+todoIndex;
     const todoLI = document.createElement("li");
     const todoText = todo.text;
@@ -49,18 +55,19 @@ function createTodoItem(todo, todoIndex ) {
                         <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                 </button>
                 `
-                const deleteButton = todoLI.querySelector(".delete-button");
-                deleteButton.addEventListener("click", ()=>{
-                    deleteTodoItem(todoIndex);
-                })
-                const checkbox = todoLI.querySelector("input");
-                checkbox.addEventListener("change", ()=>{
-                    allTodos[todoIndex].completed = checkbox.checked;
-                    saveTodos();
-                })
-                checkbox.checked = todo.completed;
-                return todoLI;
+    const deleteButton = todoLI.querySelector(".delete-button");
+    deleteButton.addEventListener("click", ()=>{
+        deleteTodoItem(todoIndex);
+    })
+    const checkbox = todoLI.querySelector("input");
+    checkbox.addEventListener("change", ()=>{
+        allTodos[todoIndex].completed = checkbox.checked;
+        saveTodos();
+    })
+    checkbox.checked = todo.completed;
+    return todoLI;
 }
+
 function deleteTodoItem(todoIndex) {
     allTodos = allTodos.filter((_, i) => i !== todoIndex);
     saveTodos();
@@ -75,4 +82,19 @@ function saveTodos() {
 function getTodos() {
     const todos = localStorage.getItem("todos") || "[]";
     return JSON.parse(todos);
+}
+
+function toggleTheme() {
+    document.body.classList.toggle('light-theme');
+    const icon = themeToggle.querySelector('i');
+    if (document.body.classList.contains('light-theme')) {
+        icon.classList.replace('fa-sun', 'fa-moon');
+    } else {
+        icon.classList.replace('fa-moon', 'fa-sun');
+    }
+}
+
+function changeColorTheme(e) {
+    const color = e.target.value;
+    document.documentElement.style.setProperty('--accent-color', color);
 }
